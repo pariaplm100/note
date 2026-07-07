876b9767bf847d4440b112c88022784b57d9857a
pariaplm100 <pariaplm100@gmail.com>
2026-07-08 01:29:22 +0330

    Fixing the navigation to the home.

--- notes/views.py
+++ notes/views.py
@@ -73,7 +73,7 @@ def login_user(request):
     
 
 def home(request):
-    return render(request, 'templates/home.html')
+    return render(request, 'home.html')
 
 def login_view(request):
     return render(request, 'accounts/login.html')
