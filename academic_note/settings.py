0400d8e786f24a2767d679a2903bf2a3df89a4cc
aryana3ri <aryana3rihosein246810@gmail.com>
2026-07-15 03:53:30 +0330

    Remove exposed SMTP credentials

--- academic_note/settings.py
+++ academic_note/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
+import os
 
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = BASE_DIR / 'media'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,8 +84,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "apdevs12@gmail.com"
-EMAIL_HOST_PASSWORD = "kogm ljbx rhkp zwwo"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
